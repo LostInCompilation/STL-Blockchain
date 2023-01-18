99d2ba0c8bc5aa5614e538c91be8f642084943b8
LostInCompilation <12819635+LostInCompilation@users.noreply.github.com>
2023-01-18 22:54:09 +0100

    Added Block impl

--- Blockchain.h
+++ Blockchain.h
@@ -35,23 +35,56 @@
 #include <functional>
 
 // Block data type
-template <typename T, typename Hasher = std::hash<T>, typename Predicate = std::equal_to<T>, typename Allocator = std::allocator<T>>
+template <typename T, typename Hasher = std::hash<T>, typename Allocator = std::allocator<T>>
 class Block
 {
 private:
+    typedef typename decltype(std::function{Hasher()})::result_type HasherResultType;
+    
+    const T transaction;
+    const uint64_t prevHash;
+    const uint64_t hash;
+    
+    template <typename H1, typename H2>
+    uint64_t PairHash(const std::pair<H1, H2>& hashPair)
+    {
+        return std::hash<H1>()(hashPair.first) ^ std::hash<H2>()(hashPair.second);
+    }
     
 public:
-    Block() {}
+    Block() = delete;
+    Block(T transaction, const uint64_t& prevHash)
+        : transaction(transaction)
+        , prevHash(prevHash)
+        , hash(CalculateHash())
+    {}
+    
+    uint64_t CalculateHash()
+    {
+        const std::pair<HasherResultType, uint64_t> pair(Hasher{}(transaction), prevHash);
+        return PairHash(pair);
+    }
+    
+    const T& GetTransaction() const { return transaction; }
+    uint64_t GetHash() const { return hash; }
+    uint64_t GetPreviousHash() const { return prevHash; }
 };
 
 // Blockchain container
-template <typename T, typename Hasher = std::hash<T>, typename Predicate = std::equal_to<T>, typename Allocator = std::allocator<T>>
-class Blockchain : public std::vector<Block<T, Hasher, Predicate, Allocator>>
+template <typename T, typename Hasher = std::hash<T>, typename Allocator = std::allocator<T>>
+requires std::is_same_v<T, typename Allocator::value_type>
+class Blockchain : public std::vector<Block<T, Hasher, Allocator>>
 {
 private:
+    typedef typename std::vector<Block<T, Hasher, Allocator>>::iterator         iterator;
+    typedef typename std::vector<Block<T, Hasher, Allocator>>::const_iterator   const_iterator;
+    
+    typedef Block<T, Hasher, Allocator> BlockType;
     
 public:
-    Blockchain() {}
+    Blockchain() { this->emplace_back(BlockType(static_cast<T>(Allocator()), 0)); }
+    Blockchain(T genesisTransaction) { this->emplace_back(BlockType(genesisTransaction, 0)); }
+    
 };
 
 #endif //BLOCKCHAIN_BLOCKCHAIN_H
